43354f140be1a0d11b4ced23b476d75a19765572
Nick Partalis <nickpartalis@hotmail.com>
2022-03-14 17:07:57 +0200

    First commit

---
+++ solver.py
@@ -0,0 +1,42 @@
+def is_valid(board, num, row, col):
+    # Check row
+    if num in board[row]:
+        return False
+
+    # Check column
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    # Check same box
+    box_row = (row // 3) * 3  # Gets the top left coordinates of the box it's in
+    box_col = (col // 3) * 3
+
+    for i in range(3):
+        for j in range(3):
+            if board[box_row + i][box_col + j] == num:
+                return False
+
+    return True
+
+
+def find_next_empty():
+    pass
+
+def solve(board):
+
+    # Find the next empty spot
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                for guess in range(1, 10):
+                    if is_valid(board, guess, row=i, col=j):
+                        board[i][j] = guess
+                        # Recursion
+                        if solve(board):
+                            return True
+                    # Backtracking
+                    board[i][j] = 0  # Runs if the next "step" has no valid answer
+                return False
+
+    return True  # Runs when there are no empty slots, so the puzzle is solved
